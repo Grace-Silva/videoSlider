b102ac109a270d3bea3a438236f8c5e57c770ce7
Grace <gracesilvaduarte@gmail.com>
2024-03-01 20:41:26 -0500

    readme y comentarios añadidos

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
-// captura de elementos:
-let items = document.querySelectorAll(".carrouselItem");    // selecciona todos los elementos del contenedor de vídeo
+// selecciona todos los contenedores de vídeo:
+let items = document.querySelectorAll(".carrouselItem");
 
-// videos:
+// selecciona los vídeos de cada uno de los contenedores:
 let videos = document.querySelectorAll(".myvid");
 
 // botones de avance:
-const leftArrow = document.getElementById("next");
-const rightArrow = document.getElementById("prev");
+const leftArrow = document.getElementById("prev"); 
+const rightArrow = document.getElementById("next");
 
-    let index = 0;
+    let index = 0; // indice para los contenedores de vídeo:
 
 // desplazar hacia la derecha (siguiente):
 function next(){
-    items[index].classList.remove("active");
-    index = (index+1) % items.length;
-    items[index].classList.add("active");
-    currentVideo=index;
-    videos[currentVideo].pause();
+    items[index].classList.remove("active"); // elimina la clase active para todos los elementos a excepción del actual
+    index = (index+1) % items.length; // avanza una posición hacia la derecha
+    items[index].classList.add("active"); // activa la visiblidad para el elemento actual
+    currentVideo = index; // necesito saber que video seleccionar, para ello tomo como referencia al indice de los contenedores
+    videos[currentVideo].pause(); // el vídeo está pausado y su avance es de 0
     videos[currentVideo].currentTime = 0;
-    videos[currentVideo].play();
+    videos[currentVideo].play(); // el vídeo está activado y su avance es automático
 }    
 
 // desplazar hacia la izquierda (atrás):
 function prev(){
     items[index].classList.remove("active");
-    index = (index-1+items.length) % items.length;
+    index = (index-1+items.length) % items.length; // avanza una posición hacia la izquierda
     items[index].classList.add("active");
     currentVideo=index;
     videos[currentVideo].pause();
@@ -32,5 +32,5 @@ function prev(){
     videos[currentVideo].play();
 }    
 
-leftArrow.addEventListener("click", next);
-rightArrow.addEventListener("click", prev);
+leftArrow.addEventListener("click", prev);
+rightArrow.addEventListener("click", next);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Video Slider
+
+En este proyecto desarrollé un carrousel de vídeo deslizante, el cual ocupará todo el ancho y alto de la pantalla.
+
+## Tecnologías:
+
+#### Html
+#### Css
+#### [Api de Audio y Vídeo de Javascript](https://www.w3schools.com/tags/ref_av_dom.asp)
+#### [Font Awesome v6.0](https://www.w3schools.com/tags/ref_av_dom.asp)
+#### [Google Fonts](https://fonts.google.com/specimen/Poppins?query=po)
+    
+![Vista previa](./preview.png)    
